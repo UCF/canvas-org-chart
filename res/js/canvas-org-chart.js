530b8ca05921418abd01c3ac9ab54140f503c8ec
Jim Barnes <james.barnes@ucf.edu>
2015-09-03 10:44:10 -0400

    Added animation loop.

--- res/js/canvas-org-chart.js
+++ res/js/canvas-org-chart.js
@@ -1,4 +1,4 @@
-function CanvasOrgChart(id, json) {
+function CanvasOrgChart(id, json, options) {
 	if (id === 'undefined') {
 		throw "Parent element must be defined must be defined";
 	}
@@ -11,21 +11,42 @@ function CanvasOrgChart(id, json) {
 	
 	this.nodes = [];
 	this.lines = [];
+	this.isPlaying = true;
 	
 	if (this.ctx === 'undefinied') {
 		this.handleNoCanvas();
 	}
 	
 	if (json !== 'undefinied') {
-		// Load json data
+		// Load data
 	} else {
 		// Load defaults
 	}
 	
 	this.setConstants();
 	
+	var self = this;
+	
+	setInterval(function() { self.play(); }, 1000 / 24);
+	
 	return this;
 }
+
+CanvasOrgChart.prototype.play = function() {
+	if (this.isPlaying) {
+		this.ctx.fillStyle = (0,0,0);
+		this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+		for (var node in this.nodes) {
+			node.update();
+			node.draw();
+		}
+		
+		for (var line in this.lines) {
+			line.update();
+			line.draw();
+		}	
+	}
+};
 
 CanvasOrgChart.prototype.handleNoCanvas = function() {
 	// ToDo: Come up with error handling for if canvas is not supported.
@@ -36,7 +57,7 @@ CanvasOrgChart.prototype.handleNoCanvas = function() {
 
 CanvasOrgChart.prototype.setConstants = function() {
 	this.canvas.width = this.parent.offsetWidth;
-	this.canvas.height = this.parent.offsetHeight;
+	this.canvas.height = this.canvas.width / 4 * 3;
 	this.gridUnit = {
 		x: Math.floor(this.canvas.width / 15),
 		y: Math.floor(this.canvas.height / 8)
