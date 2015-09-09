f89d78d35f2ebf2010deaa1766b189179647f620
Keegan Berry <kberry@cmd25kx0a1dnmp.net.ucf.edu>
2015-09-09 11:03:28 -0400

    replaced canvas with svg

--- res/js/canvas-org-chart.js
+++ res/js/canvas-org-chart.js
@@ -1,7 +1,7 @@
 function CanvasOrgChart(id, data, options) {
-	
+
 	var self = this;
-	
+
 	self.defaultOptions = function() {
 		return {
 			backgroundColor: '#fff',
@@ -23,88 +23,100 @@ function CanvasOrgChart(id, data, options) {
 			lineColor: '#000'
 		};
 	};
-	
+
 	self.handleNoCanvas = function() {
 		// ToDo: Come up with error handling for if canvas is not supported.
 		return {
 			message: "Canvas is not supported by this browser."
 		};
 	};
-	
+
 	self.render = function() {
 		for (var n in self.nodes) {
 			self.nodes[n].render();
 		}
-		
+
 		for (var l in self.lines) {
 			self.lines[l].render();
 		}
 	};
-	
+
 	self.setConstants = function() {
-		self.canvas.width = self.parent.offsetWidth;
-		self.canvas.height = self.canvas.width / 4 * 3;
-		var canvasRect = self.canvas.getBoundingClientRect();
-		self.overlay.style.top = canvasRect.top + 'px';
-		self.overlay.style.left = canvasRect.left + 'px';
-		self.overlay.style.width = canvasRect.width + 'px';
-		self.overlay.style.height = canvasRect.height + 'px';
-		self.rect = self.canvas.getBoundingClientRect();
+
+		var set_width = self.parent.offsetWidth;
+		var set_height = set_width / 4 * 3;
+
+		self.container.setAttribute('width', set_width);
+		self.container.setAttribute('height', set_height);
+
+		// var canvasRect = self.canvas.getBoundingClientRect();
+		// self.overlay.style.top = canvasRect.top + 'px';
+		// self.overlay.style.left = canvasRect.left + 'px';
+		// self.overlay.style.width = canvasRect.width + 'px';
+		// self.overlay.style.height = canvasRect.height + 'px';
+		//self.rect = self.canvas.getBoundingClientRect();
 		self.gridUnit = {
-			x: Math.floor(self.canvas.width / 15),
-			y: Math.floor(self.canvas.height / 15)
+			x: Math.floor(set_width / 15),
+			y: Math.floor(set_height / 15)
 		};
+		console.log(self.gridUnit);
 	};
-	
+
 	self.__init__ = function() {
 		if (id === 'undefined') {
 			throw "Parent element must be defined must be defined";
 		}
-		
-		self.canvas = document.createElement('canvas');
-		self.ctx = self.canvas.getContext('2d');
-		
+
+		//self.canvas = document.createElement('canvas');
+		self.container = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+		//self.ctx = //self.canvas.getContext('2d');
+
 		self.overlay = document.createElement('div');
 		self.overlay.style.position = 'absolute';
-		
+		self.container.style.position = 'absolute';
+		self.container.style.left = '0';
+		self.container.style.top = '0';
+
+
 		self.parent = document.getElementById(id);
-		self.parent.appendChild(self.canvas);
+		self.parent.style.position = 'relative';
+		self.parent.appendChild(self.container);
 		self.parent.appendChild(self.overlay);
-		
+
 		self.options = self.defaultOptions();
-		
+
 		// Merge default options with options passed in.
 		if (options !== 'undefined') {
 			for (var key in options) {
 				self[key] = options[key];
 			}
 		}
-		
+
 		self.nodes = [];
 		self.lines = [];
-		
+
 		if (data !== 'undefinied') {
 			for (var node in data.nodes) {
 				self.nodes.push(new ChartNode(self, data.nodes[node]));
 			}
-			
+
 			for (var line in data.lines) {
 				self.lines.push(new ChartLine(self, data.lines[line]));
 			}
 		} else {
 			// do something else...
 		}
-		
+
 		if (self.ctx === 'undefinied') {
 			self.handleNoCanvas();
 		}
-		
+
 		self.setConstants();
-		
+
 		self.render();
-		
+
 		return self;
 	};
-	
+
 	return self.__init__();
 }
--- res/js/chart-line.js
+++ res/js/chart-line.js
@@ -1,37 +1,43 @@
 function ChartLine(chart, options) {
 	var self = this;
-	
+
 	self.chart = chart;
 	if (options !== 'undefined') {
 		for (var option in options) {
 			self[option] = options[option];
 		}
 	}
-	
+
 	self.render = function() {
-		var ctx = self.chart.ctx;
+		//var ctx = self.chart.ctx;
 		var gux = self.chart.gridUnit.x;
 		var guy = self.chart.gridUnit.y;
-		
-		ctx.save();
-		ctx.beginPath();
-		
-		ctx.strokeStyle = self.chart.lineColor;
-		
+
+		//ctx.save();
+		//ctx.beginPath();
+
+		//ctx.strokeStyle = self.chart.lineColor;
+
+		var newLine = document.createElementNS('http://www.w3.org/2000/svg', 'line');
+			newLine.setAttribute('class', 'org-line');
+
 		if (self.style === 'dotted') {
-			ctx.setLineDash([3, 9]);
+			//ctx.setLineDash([3, 9]);
+			newLine.classList.add('dotted');
 		}
-		
+
 		for (var idx in self.waypoints) {
 			var waypoint = self.waypoints[idx];
-			ctx.moveTo(waypoint.from.x * gux, waypoint.from.y * guy);
-			ctx.lineTo(waypoint.to.x * gux, waypoint.to.y * guy);
-			ctx.stroke();
+			newLine.setAttribute('x1', waypoint.from.x * gux);
+			newLine.setAttribute('y1', waypoint.from.y * guy);
+			newLine.setAttribute('x2', waypoint.to.x * gux);
+			newLine.setAttribute('y2', waypoint.to.y * guy);
+			chart.container.appendChild(newLine);
 		}
-		
-		ctx.closePath();
-		ctx.restore();
+
+		//ctx.closePath();
+		//ctx.restore();
 	};
-	
+
 	return self;
 }
